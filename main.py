1baa92291a60612b8192884f3b770fe61bc81bea
kychen@davidson.edu <kyle@Mutians-MacBook-Pro.local>
2016-12-18 02:29:54 +0800

    first commit

--- main.py
+++ main.py
@@ -1,5 +1,29 @@
 from openpyxl import Workbook
 import time, ConfigParser
+def init_ini():
+	config = ConfigParser.ConfigParser()
+	config.add_section("TIME")
+	config.add_section("OPTION")
 
+	config.set("TIME","year","0000")
+	config.set("TIME","mon","00")
+	config.set("TIME","mday","00")
+	config.set("TIME","wday","0")
+	config.set("OPTION","moption","16")
+	config.set("OPTION","dblance",0)
+	config.set("OPTION","rmeal",0)
+
+	config.write(open('config.ini',"w"))
+
+def update_time():
+	config = ConfigParser.ConfigParser()
+	config.read('config.ini')
+	cur = time.localtime()
+	config.set("TIME","year",cur.tm_year)
+	config.set("TIME","mon",cur.tm_mon)
+	config.set("TIME","mday",cur.tm_mday)
+	config.set("TIME","wday",cur.tm_wday)
+	config.write(open('config.ini',"r+"))
 if __name__ == '__main__':
-    print(time.localtime().tm_year)
+    init_ini()
+    update_time()
